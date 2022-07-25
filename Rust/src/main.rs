0de6dbb0d26d4e1cc4b91dff328152f63cb95287
na2na-p <na2na@na2na.dev>
2022-07-25 00:42:56 +0000

    Update

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
     // println!("Hello, world!");
-	// 引数を配列に格納する
-	let args = std::env::args().collect::<Vec<_>>();
+    // 引数を配列に格納する
+    let args = std::env::args().collect::<Vec<_>>();
 
-	// 引数を表示する
-	println!("args: {:?}", args);
-	println!("args: {:?}", args[1]);
-	println!("args: {:?}", args[2]);
+    // 引数を表示する
+    println!("args: {:?}", args);
+    println!("args: {:?}", args[1]);
+    println!("args: {:?}", args[2]);
 
-	if args.len() < 2 || args.len() > 4 {
-		panic!("正しい表示形式ではありません。CIDR形式と他の形式を混在させることはできません。");
-	}
+    if args.len() < 2 || args.len() > 4 {
+        panic!("正しい表示形式ではありません。CIDR形式と他の形式を混在させることはできません。");
+    }
 }
