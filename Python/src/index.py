605b7923f09dc6a518363876f733ddf0232be990
na2na-p <na2na@na2na.dev>
2022-07-25 09:31:57 +0900

    Update

--- Python/src/index.py
+++ Python/src/index.py
@@ -4,4 +4,4 @@ args = sys.argv[1:]
 
 
 if len(args) < 2 or len(args) > 4:
-	raise Exception('正しい表示形式ではありません。CIDR形式と他の形式を混在させることはできません。')
+    raise Exception('正しい表示形式ではありません。CIDR形式と他の形式を混在させることはできません。')
