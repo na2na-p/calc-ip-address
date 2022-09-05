9c771e818aa9b7e071d3888e87c29902722e6ac3
na2na-p <na2na@na2na.dev>
2022-09-05 12:06:33 +0900

    Fix

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -7,7 +7,7 @@ import { Output } from '@modules/Output.js';
 
 // 引数が2個ならtrue,4個ならfalseを返す。
 // そうでなければErrorをthrowする。
-if (args.length < 2 || args.length > 4) {
+if (args.length < 2 || args.length > 4 || (args.length == 2 && (!(args[0].includes('/')) || !(args[1].includes('/'))))) {
 	throw new Error('正しい表示形式ではありません。CIDR形式と他の形式を混在させることはできません。');
 }
 
